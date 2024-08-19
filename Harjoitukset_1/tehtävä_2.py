d9c4ed7e66264d43a7ce3160118f0c6dcce5eff6
LAPTOP-UMI8O26I\nordm <nordmanjerry@gmail.com>
2024-08-19 15:55:53 +0300

    Tehtävät 2. 1-2

--- Harjoitukset_1/tehtävä_2.py
+++ Harjoitukset_1/tehtävä_2.py
@@ -4,3 +4,11 @@
 print("Mikä sinun nimesi on?")
 nimi = input()
 print("Hei " + nimi + "!")
+
+#2
+print("Anna ympyrän säde:")
+ympyranSade = float(input())
+pii = 3.141
+ympyranPintaAla = ympyranSade * ympyranSade * pii
+print(ympyranPintaAla)
+
