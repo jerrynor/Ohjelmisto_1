b29d7977db2da2add6c426ee7895b92160b9a52e
LAPTOP-UMI8O26I\nordm <nordmanjerry@gmail.com>
2024-08-20 11:28:56 +0300

    Tehtävä2. Tehtävä #4

--- Harjoitukset_1/tehtävä_2.py
+++ Harjoitukset_1/tehtävä_2.py
@@ -1,16 +1,17 @@
 #Muuttujat ja vuorovaikutteiset ohjelmat
 
+"""
 #1.
-#print("Mikä sinun nimesi on?")
-#nimi = input()
-#print("Hei " + nimi + "!")
+print("Mikä sinun nimesi on?")
+nimi = input()
+print("Hei " + nimi + "!")
 
 #2
-#print("Anna ympyrän säde:")
-#ympyranSade = float(input())
-#pii = 3.141
-#ympyranPintaAla = ympyranSade * ympyranSade * pii
-#print(ympyranPintaAla)
+print("Anna ympyrän säde:")
+ympyranSade = float(input())
+pii = 3.141
+ympyranPintaAla = ympyranSade * ympyranSade * pii
+print(ympyranPintaAla)
 
 #3
 kanta = float(input("Anna suorakulmion kannan, pituus: "))
@@ -21,7 +22,24 @@ suoraKulmionPintaAla = kanta * korkeus
 print("Suorakulmion piiri on: " + str(suoraKulmionPiiri))
 print("Suorakulmion pinta-ala on: " + str(suoraKulmionPintaAla) +" m2")
 
+
 #4
+luku1 = int(input("Anna ensimmäinen kokonaisluku: "))
+luku2 = int(input("Anna toinen kokonaisluku: "))
+luku3 = int(input("Anna kolmas kokonaisluku: "))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
+"""
+
+#5
+
+
 
 
 
