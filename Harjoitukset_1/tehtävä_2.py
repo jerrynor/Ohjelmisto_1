fa41a755e5313e7db18e01fe2db9ad9bd0653c5c
LAPTOP-UMI8O26I\nordm <nordmanjerry@gmail.com>
2024-08-20 09:03:29 +0300

    Tehtävä2. Tehtävä #3

--- Harjoitukset_1/tehtävä_2.py
+++ Harjoitukset_1/tehtävä_2.py
@@ -1,14 +1,27 @@
 #Muuttujat ja vuorovaikutteiset ohjelmat
 
 #1.
-print("Mikä sinun nimesi on?")
-nimi = input()
-print("Hei " + nimi + "!")
+#print("Mikä sinun nimesi on?")
+#nimi = input()
+#print("Hei " + nimi + "!")
 
 #2
-print("Anna ympyrän säde:")
-ympyranSade = float(input())
-pii = 3.141
-ympyranPintaAla = ympyranSade * ympyranSade * pii
-print(ympyranPintaAla)
+#print("Anna ympyrän säde:")
+#ympyranSade = float(input())
+#pii = 3.141
+#ympyranPintaAla = ympyranSade * ympyranSade * pii
+#print(ympyranPintaAla)
+
+#3
+kanta = float(input("Anna suorakulmion kannan, pituus: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+
+suoraKulmionPiiri = kanta + korkeus * 2
+suoraKulmionPintaAla = kanta * korkeus
+print("Suorakulmion piiri on: " + str(suoraKulmionPiiri))
+print("Suorakulmion pinta-ala on: " + str(suoraKulmionPintaAla) +" m2")
+
+#4
+
+
 
